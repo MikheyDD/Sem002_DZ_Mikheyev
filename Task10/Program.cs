42698258472a7505a1fd414448f7f5b7aea61199
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-16 21:23:48 +0300

    task13

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,5 +1,4 @@
-﻿Console.WriteLine("Введите трехзначное число");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите трехзначное число");
 if (99 < num && num < 999)
 {
     Console.WriteLine(num / 10 % 10);
---
+++ Task13_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int numLength = num;
+while (numLength != 0)
+    {
+        numLength = numLength / 10;
+        count++;
+    }
+if (count < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (count != 3)
+    {
+        num = num / 10;
+        count = count - 1;
+    }
+Console.WriteLine(num % 10); 
+}
