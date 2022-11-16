921411eb40c1e94b07b733e8b9c738c1225e9803
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-16 22:15:16 +0300

    task15

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1 || num > 7)
+{
+    Console.WriteLine("Число вне заданного диапазона");
+}
+else
+{
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    if (num >= 1 && num <= 5)
+    {
+        Console.WriteLine("Будний день");
+    }
+}
