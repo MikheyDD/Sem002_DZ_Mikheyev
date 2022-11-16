10de8ad34a1b313af9bc9fa2010ae24f43ea22f4
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-16 22:18:31 +0300

    task10_1

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,5 @@
-Console.WriteLine("Введите трехзначное число");
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
 if (99 < num && num < 999)
 {
     Console.WriteLine(num / 10 % 10);
