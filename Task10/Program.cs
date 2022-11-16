408d0b4e3f8dfe907cd134e0fb0d107b866b3e51
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-16 20:12:39 +0300

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (99 < num && num < 999)
+{
+    Console.WriteLine(num / 10 % 10);
+}
+else 
+{
+    Console.WriteLine("Число не трехзначное");
+}
